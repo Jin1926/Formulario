6d24b6d514f63127d342c00160507811319a18b6
Jin1926 <jinfer42@gmail.com>
2024-10-18 16:05:04 -0500

    Creacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CrearUsuarioComponent } from './crear-usuario/crear-usuario.component';
+import { HeadComponent } from './head/head.component';
+import { FooterComponent } from './footer/footer.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CrearUsuarioComponent, HeadComponent, FooterComponent, PrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'formativa-corte2';
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CrearUsuarioComponent } from './crear-usuario/crear-usuario.component';
+import { HeadComponent } from './head/head.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'CrearUsuarios',
+        component: CrearUsuarioComponent
+    },
+    {
+        path: '',
+        component: HeadComponent
+    }
+];

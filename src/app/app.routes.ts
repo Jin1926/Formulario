4a1ef27eefbbc58effc0b681506aec22642cdff1
Jin1926 <jinfer42@gmail.com>
2024-10-18 22:29:11 -0500

    formulario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { CrearUsuarioComponent } from './crear-usuario/crear-usuario.component';
-import { HeadComponent } from './head/head.component';
 import { PrincipalComponent } from './principal/principal.component';
 
 export const routes: Routes = [
--- src/app/crear-usuario/crear-usuario.component.ts
+++ src/app/crear-usuario/crear-usuario.component.ts
@@ -1,12 +1,40 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-usuario',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './crear-usuario.component.html',
   styleUrl: './crear-usuario.component.css'
 })
 export class CrearUsuarioComponent {
 
+  usuarios = {
+    nombre: "",
+    apellido: "",
+    edad: "",
+    identificacion: "",
+    fechaNacimiento: "",
+    genero: "",
+    pais: ""
+
+  }
+  mensaje: string = "";
+  resultado: string = "";
+
+  onSubmit(form: NgForm) {
+
+    this.mensaje = "";
+    this.resultado = '';
+
+    if (form.invalid) {
+      this.mensaje = "Todos los campos obligatorios.";
+      return;
+    }
+
+    this.resultado = 'Formulario enviado exitosamente!';
+    //console.log('Usuario creado:', this.usuarios);
+  }
 }

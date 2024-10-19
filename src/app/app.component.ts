da002839932758c93e10401cd3051cff8287972f
Jin1926 <jinfer42@gmail.com>
2024-10-19 13:09:21 -0500

    Validaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CrearUsuarioComponent } from './crear-usuario/crear-usuario.component';
 import { HeadComponent } from './head/head.component';
-import { FooterComponent } from './footer/footer.component';
 import { PrincipalComponent } from './principal/principal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CrearUsuarioComponent, HeadComponent, FooterComponent, PrincipalComponent],
+  imports: [RouterOutlet, CrearUsuarioComponent, HeadComponent, PrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
